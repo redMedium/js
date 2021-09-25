b095b4b58877ca806b00059e26bf37d3238d65e8
redMedium <d1149511@gmail.com>
2021-09-25 12:23:53 +0300

    clone Project_1 to GitHub

---
+++ Project_1/js/js.js
@@ -0,0 +1,6 @@
+var bodyElements = document.body.getElementsByTagName("div");
+for (var i = 1; i < bodyElements.length - 1; i++) {
+    bodyElements[i].style.display = "none";
+}
+
+function addColumnView()
